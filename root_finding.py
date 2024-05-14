deeb248f7f070a4e2cf15505232f9432dea5cb26
Noah Royce <55113359+nroyce7@users.noreply.github.com>
2024-05-14 13:01:28 -0500

    Update root_finding.py
    Added more methods

--- root_finding.py
+++ root_finding.py
@@ -1,5 +1,41 @@
 import numpy as np
 
+def bisection_method(f, a, b, tol = 1e-4, max_itr = 100):
+    if f(a)*f(b) > 0:
+        print('Root not in interval')
+        return -1
+
+    for i in range(max_itr):
+        c = (a+b)/2
+        if f(a)*f(c) < 0:
+            b = c
+        else:
+            a = c
+
+        if abs(f(c)) < tol:
+            print(f'Root within tolerance found after {i} iterations.')
+            return c
+    print('Maximum iterations reached')
+    return c
+
+def false_position(f, a, b, tol = 1e-4, max_itr = 100):
+    if f(a)*f(b) > 0:
+        print('Root not in interval')
+        return -1
+
+    for i in range(max_itr):
+        c = (a*f(b) - b*f(a))/(f(b) - f(a))
+
+        if f(a)*f(c) < 0:
+            b = c
+        else:
+            a = c
+
+        if abs(f(c)) < tol:
+            print(f'Root within tolerance found after {i} iterations.')
+            return c
+    print('Maximum iterations reached')
+    return c
 
 def newton_method(f, f_prime, x_0, tol = 1e-4, max_itr = 100):
     x = x_0
@@ -14,3 +50,15 @@ def newton_method(f, f_prime, x_0, tol = 1e-4, max_itr = 100):
         
     print('Maximum iterations reached')
     return x
+
+def secant_method(f, x_0, x_1, tol = 1e-4, max_itr = 100):
+    for i in range(max_itr):
+        x_2 = x_1 - f(x_1)*((x_1 - x_0)/(f(x_1) - f(x_0)))
+        x_0, x_1 = x_1, x_2
+
+        if abs(f(x_1)) < tol:
+            print(f'Root within tolerance found after {i} iterations.')
+            return x_1
+
+    print('Maximum iterations reached')
+    return x_1
