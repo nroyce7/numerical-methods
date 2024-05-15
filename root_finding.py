c2a3e9877ec13413bddf796b1e6f0b87bcc1f0fe
Noah <noah.royce7@gmail.com>
2024-05-15 12:37:56 -0500

    Adding Files

--- root_finding.py
+++ root_finding.py
@@ -40,7 +40,7 @@ def false_position(f, a, b, tol = 1e-4, max_itr = 100):
 def newton_method(f, f_prime, x_0, tol = 1e-4, max_itr = 100):
     x = x_0
     for i in range(max_itr):
-        x_n = x - (y/y_p)
+        x_n = x - (f(x)/f_prime(x))
 
         if abs(x_n - x) < tol:
             print(f'Root within tolerance found after {i} iterations.')
---
+++ testing_env.py
@@ -0,0 +1,52 @@
+from ode_solvers import *
+from root_finding import *
+import matplotlib.pyplot as plt
+
+
+'''
+def f(t, y):
+    return np.sin(10*t)
+
+t = np.linspace(0, np.pi, 1000)
+n = 50
+sol_e = euler(f, 1, 0, np.pi, n)
+sol_r = rk4(f, 1, 0, np.pi, n)
+
+plt.plot(sol_e[:,0], sol_e[:,1], sol_r[:,0], sol_r[:,1])
+plt.plot(t,-1/10*np.cos(10*t)+11/10)
+plt.legend(['Euler', 'RK4', 'Exact'])
+plt.show()
+'''
+
+
+def f(x):
+    return x**3 - x - 2
+
+def f_prime(x):
+    return 3*x**2 - 1
+
+print('\n\n\n\n\n')
+
+print('Newton Method')
+print('----------------------------')
+x = newton_method(f, f_prime, 2)
+print(x)
+print('\n')
+
+print('Bisection Method')
+print('----------------------------')
+x = bisection_method(f, 1, 2)
+print(x)
+print('\n')
+
+print('False Position')
+print('----------------------------')
+x = false_position(f, 1, 2)
+print(x)
+print('\n')
+
+print('Secant Method')
+print('----------------------------')
+x = secant_method(f, 1, 2)
+print(x)
+print('\n')
